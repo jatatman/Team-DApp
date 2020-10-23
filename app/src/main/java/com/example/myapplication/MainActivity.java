5461226278b03156542b84367c61745ccb0d4446
nivek1028 <nivek1028@hotmail.com>
2020-10-23 17:18:19 -0600

    needs fix

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,31 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final ImageView dog = (ImageView) findViewById(R.id.Image);
         Button like = (Button) findViewById(R.id.Like);
+        int numofpics = 10;
+        int count = 1;
+        String picname = "puppy";
 
-        Button dislike = (Button) findViewById(R.id.Dislike);
-        //
-        like.setOnClickListener(new View.OnClickListener()
+        while(count <= numofpics)
         {
-            @Override
-            public void onClick(View v) {
-                Log.i("MYapp", "This is log message");
-                openNodogs();
+            if(count == numofpics)
+            {
+                openNoDogs();
+                break;
             }
-        });
+            Button dislike = (Button) findViewById(R.id.Dislike);
+            like.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("MYapp", "This is log message");
+                    // openNoDogs();
+                    dog.setImageResource(R.drawable.puppy);
 
-        dislike.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Log.i("MYapp", "This is log message");
-                openNodogs();
-            }
-        });
+                }
+            });
+
+            dislike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("MYapp", "This is log message");
+                    //openNoDogs();
+                    dog.setImageResource(R.drawable.puppies);
+                }
+            });
+            count++;
+        }
     }
-    public void openNodogs() {
+    public void openNoDogs() {
         Intent intent = new Intent(this, nodogs.class);
+        startActivity(intent);
+    }
+
+    public void nextPicture()
+    {
+        Intent intent = new Intent(this, nextPicture.class);
         startActivity(intent);
     }
 }

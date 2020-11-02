781710308ae9a941e075d3d351aa21e0cd2841c8
jtatman <31909713+jatatman@users.noreply.github.com>
2020-11-02 13:14:05 -0700

    Added ipynb for web scraping.
    Added web_scraping ipynb and reorganized code in database_setup.

--- app/src/main/java/com/example/myapplication/Backend.java
+++ app/src/main/java/com/example/myapplication/Backend.java
@@ -1,4 +1,12 @@
 package com.example.myapplication;
 
+import java.net.HttpURLConnection;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
 public class Backend {
+    /*
+    URL url = new URL("http://localhost:8000");
+    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    */
 }
